1e40b7a086f9b60a3355000e1ee9ded36f084371
Amitesh Singh <itsmeamitesh2001@gmail.com>
2025-09-10 00:34:35 +0530

    Update 03_rfm_scoring.sql

--- sql_scripts/03_rfm_scoring.sql
+++ sql_scripts/03_rfm_scoring.sql
@@ -1,3 +1,17 @@
+/*************************************************************************************************
+-- PROJECT: RFM Customer Segmentation Analysis
+-- SCRIPT: 03_rfm_scoring.sql
+-- DESCRIPTION: This script scores customers on a 1-4 scale for each RFM dimension based on quartiles.
+--              This scoring allows for easy comparison between customers.
+-- METHODOLOGY:
+--    - Uses the NTILE(4) window function to create four equal-sized groups.
+--    - A score of 4 is always the best (e.g., most recent, most frequent, highest spending).
+-- AUTHOR: Amitesh Kumar Singh
+-- DATE: September 10, 2025
+*************************************************************************************************/
+
+
+
 /*
   Your goal is to create a new table, customer_rfm_scores, which contains all the data from your previous table, plus the new R, F, and M scores.
 
